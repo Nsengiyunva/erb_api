70bd8155e353a3517e5357b8b6e27037260ae846
Nsengiyunva <kingbecks07@gmail.com>
2025-12-10 14:06:24 +0300

    updates the app

--- src/server.ts
+++ src/server.ts
@@ -3,18 +3,24 @@ import dotenv from "dotenv";
 import engineers_routes  from "./routes/engineer_routes"
 import { sequelize, connectDB } from "./config/database";
 import cors from "cors";
-import listEndpoints from 'express-list-endpoints';
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: [
+      "http://localhost:3001",
+      "http://localhost:3000",
+      "https://data.erb.go.ug",
+      "https://sit.erb.go.ug"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+  }));
+app.options("*", cors());  
 app.use(express.json());
 
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/farmers", farmerRoutes);
-// app.use("/api/nfa", nfaMainRoutes);
 app.use( "/api/engineers", engineers_routes )
 
 
